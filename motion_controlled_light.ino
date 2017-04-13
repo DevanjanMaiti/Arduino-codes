ed4431d3cac320deeb65c2437feae2e3f78c6974
Devanjan Maiti <devanjan008@gmail.com>
2017-04-13 19:59:06 +0530

    Simpler working version
    Have to make the code better later.

--- motion_controlled_light.ino
+++ motion_controlled_light.ino
@@ -1,47 +1,35 @@
-#define LDR_in A0
-//#define Front_PIR_in 2
-#define Back_PIR_in 3  // UNUSED
-#define Bypass_Switch_in 7
-#define Relay_ctrl_out 13
-
-#define LDR_Threshold 90
-#define ON_Time 120000
-
 void setup() {
 // Initial Setup:
 // LDR Input
-  pinMode(LDR_in,INPUT);
+  pinMode(A0,INPUT);
 
 // Front PIR Sensor Input (only)
   pinMode(2,INPUT);
 
 // Bypass Switch Input 
-  pinMode(Bypass_Switch_in,INPUT);
+  pinMode(7,INPUT);
 
 // Relay Control Output
-  pinMode(Relay_ctrl_out,OUTPUT);
-  digitalWrite(Relay_ctrl_out,LOW);
-Serial.begin(9600);
+  pinMode(12,OUTPUT);
+  digitalWrite(12,LOW);
 }
 
 void loop() {
 // Main code:
-Serial.println(digitalRead(2));
-//  if(digitalRead(Bypass_Switch_in)) {
+  if(digitalRead(7)) {
     if(digitalRead(2)) {  // || digitalRead(Back_Pir_in)
-      if(analogRead(LDR_in) < LDR_Threshold) {
-          digitalWrite(Relay_ctrl_out,HIGH);
-          delay(ON_Time);
-          while(digitalRead(2)); // || digitalRead(Back_Pir_in)
+      if(analogRead(A0) < 90) {
+        digitalWrite(12,HIGH);
+        delay(180000);
+        while(digitalRead(2)); // || digitalRead(Back_Pir_in)
       }
     }
     else
     {
-      digitalWrite(Relay_ctrl_out,LOW);
+      digitalWrite(12,LOW);
     }
-//  }
-//  else {
-//  digitalWrite(Relay_ctrl_out,HIGH);
-//  }
-delay(100);
+  }
+  else {
+    digitalWrite(12,HIGH);
+  }     
 }
